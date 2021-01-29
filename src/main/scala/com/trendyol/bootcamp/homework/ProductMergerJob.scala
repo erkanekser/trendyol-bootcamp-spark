acc232d3f39fc155ea20558d122df84b6959ea1b
erkanekser <erknekser@gmail.com>
2021-01-29 10:57:33 +0300

    Add ProductMergerJob homework

--- src/main/scala/com/trendyol/bootcamp/homework/ProductMergerJob.scala
+++ src/main/scala/com/trendyol/bootcamp/homework/ProductMergerJob.scala
@@ -1,5 +1,9 @@
 package com.trendyol.bootcamp.homework
 
+import org.apache.spark.sql.{Encoders, SaveMode, SparkSession}
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.functions.dense_rank
+
 object ProductMergerJob {
 
   def main(args: Array[String]): Unit = {
@@ -16,6 +20,48 @@ object ProductMergerJob {
     * Note: You can use SQL, dataframe or dataset APIs, but type safe implementation is recommended.
     */
 
+    val spark = SparkSession
+      .builder()
+      .master("local")
+      .appName("Product Merger Job")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+    import spark.implicits._
+
+    val initialDataSchema = Encoders.product[commonDataSchema].schema
+    val initialData = spark.read
+      .schema(initialDataSchema)
+      .json("data/homework/initial_data.json")
+      .as[commonDataSchema]
+
+    val cdcDataSchema = Encoders.product[commonDataSchema].schema
+    val cdcData = spark.read
+      .schema(cdcDataSchema)
+      .json("data/homework/cdc_data.json")
+      .as[commonDataSchema]
+
+    cdcData.show()
+
+    val w = Window
+      .partitionBy('id)
+      .orderBy('timestamp desc)
+
+    val last_df = initialData
+      .union(cdcData)
+      .withColumn("rank",dense_rank() over w)
+      .filter("rank = 1")
+      .orderBy("timestamp")
+      .drop("rank")
+
+    last_df
+      .repartition(1)
+      .write
+      .mode(SaveMode.Overwrite)
+      .json("data/homework/last_df")
+
   }
 
 }
+
+case class commonDataSchema(id: Int, name: String, category: String, brand: String, color: String, price: Double, timestamp: Long)
